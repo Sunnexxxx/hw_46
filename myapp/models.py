b75bc65095737c2ae4d88c7baef52c8c820ebba1
Sunnexxxx <artykovhondamir@gmail.com>
2024-04-12 09:15:41 +0300

    Initial commit

---
+++ myapp/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+
+class AbstractProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class UserProfile(AbstractProfile):
+    pass
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.userprofile.save()
+
+
+@receiver(post_save, sender=UserProfile)
+def user_profile_saved(sender, instance, created, **kwargs):
+    if created:
+        print("Профиль пользователя создан.")
+    else:
+        print("Профиль пользователя обновлен.")
